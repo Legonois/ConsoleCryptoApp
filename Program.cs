5920d3f73e78b1e3bc919d538fca325df46350ac
Legonois <wesley.baldwin@outlook.com>
2021-11-26 16:43:18 -0600

    Added more commands
    Added commands:
    Quit
    Hash
    Hashfile(not implmented)
    OutputFile(not implemented)
    
    Added a default catchall
    Added a way to quit the program
    
    Fixed case sensitive issue

--- Program.cs
+++ Program.cs
@@ -10,28 +10,78 @@ namespace ConsoleHashingApp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("+------------------------------------------------+");
+            Console.WriteLine("|   Welcome to ConsoleHashingApp                 |");
+            Console.WriteLine("|   Made by Lunar HUE                            |");
+            Console.WriteLine("|   LunarHUE.com                                 |");
+            Console.WriteLine("|                                                |");
+            Console.WriteLine("|   Version Alpha 1.01                           |");
+            Console.WriteLine("+------------------------------------------------+");
+            Console.WriteLine("Enter command. Use help for a list of commands");
+
+            Console.WriteLine();
+
+            String Command = Console.ReadLine().ToLower();
+
+            while (!Command.ToLower().Equals("quit"))
+            {
+                switch (Command)
+                {
+                    case string s when s.ToLower().StartsWith("help"):
+                        Console.WriteLine("");
+                        Console.WriteLine("Command     | Syntax                 | Usage");
+                        Console.WriteLine("---------------------------------------------------------------------------------------");
+                        Console.WriteLine("Hash        | Hash (plaintext)       | Plaintext into a Hash using SHA256");
+                        Console.WriteLine("Quit        | Quit                   | Quit the program");
+                        Console.WriteLine("Hashfile    | Hashfile (Directory)   | Files into a Hash using SHA256");
+                        Console.WriteLine("OutputFile  | OutputFile (Directory) | Output the commands and outputs of this instance");
+                        Console.WriteLine();
+                        Console.WriteLine("Commands are NOT case sensitive");
+                        Console.WriteLine();
+                        break;
+
+                    case string s when s.ToLower().StartsWith("hash "):
+                        string output = Hash(s.Substring("hash ".Length));
+                        
+                        Console.WriteLine($"Hast: {output}");
+                        Console.WriteLine();
+
+                        break;
+
+                    case string s when s.ToLower().StartsWith("hashfile "):
+                        Console.WriteLine($"Command (Hashfile) not yet implimented");
+                        Console.WriteLine("Please wait for an updated version");
+                        Console.WriteLine();
+
+                        break;
+
+
+                    default:
+                        Console.WriteLine("Invalid command. Use help for list of commands");
+                        Console.WriteLine();
+                        break;
+                }
+                Command = Console.ReadLine();
+            }
+
             string ConsoleLine = String.Empty;
 
-            using (SHA256 mySHA256 = SHA256.Create())
-            {
-                while (!string.IsNullOrWhiteSpace(ConsoleLine = Console.ReadLine()))
-                {
-                    Console.WriteLine();
-                    Console.WriteLine($"Orig: {ConsoleLine}");
 
-                    byte[] myArray = Encoding.ASCII.GetBytes(ConsoleLine);
-
-                    byte[] hashValue = mySHA256.ComputeHash(myArray);
-
-                    Console.WriteLine($"Hash: {ByteArrayToString(hashValue)}");
-                    Console.WriteLine();
-                };
-            }
         }
 
         public static string ByteArrayToString(byte[] ba)
         {
             return BitConverter.ToString(ba).Replace("-", "");
+        }
+
+        public static string Hash(string input)
+        {
+            using (SHA256 mySHA256 = SHA256.Create())
+            {
+                byte[] myArray = Encoding.ASCII.GetBytes(input);
+                byte[] hashValue = mySHA256.ComputeHash(myArray);
+                return ByteArrayToString(hashValue);
+            }
         }
     }
 }
